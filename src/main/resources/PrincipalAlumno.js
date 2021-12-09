9005a0196100fce06db247aa235fb181a95c0bcc
Francisco <frankdanielsanchez@gmail.com>
2021-12-08 21:21:01 -0600

    Queda penciente relacionar al maestro con sus encuestas :D

---
+++ src/main/resources/PrincipalAlumno.js
@@ -0,0 +1,11 @@
+window.onload(function() {
+
+    let divEncuesta = document.createElement("div");
+    let label = document.createElement("label");
+    let labelProfesor = document.createElement("label");
+    let btnContestar = document.createElement("button");
+
+    
+
+});
+
