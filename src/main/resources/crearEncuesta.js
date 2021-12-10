d11af2aeecb8abcf07249d05e7474b806522ccfc
Francisco <frankdanielsanchez@gmail.com>
2021-12-10 13:50:32 -0600

    falta desplegar el video de youtube

--- src/main/resources/crearEncuesta.js
+++ src/main/resources/crearEncuesta.js
@@ -9,7 +9,10 @@ btnAgregarPregunta.addEventListener("click", function() {
     let labelPregunta = document.createElement("label");
     let btnBorrar = document.createElement("button");
     let btnRespuestas = document.createElement("button");
-    let videoPregunta = document.createElement("video");
+    let videoPregunta = document.createElement("embed");
+
+    //<object style="width:100%;height:100%;width: 820px; height: 461.25px; float: none; 
+    //clear: both; margin: 2px auto;" data="http://www.youtube.com/embed/GlIzuTQGgzs"></object>
 
     pregunta.style.border = "thin solid #00CC66";
     pregunta.style.marginTop = "20px";
@@ -18,9 +21,13 @@ btnAgregarPregunta.addEventListener("click", function() {
     pregunta.style.justifyContent = "center";
     pregunta.style.alignItems = "center";
 
-    videoPregunta.controls = true;
-    videoPregunta.style.width = "400px";
-    videoPregunta.style.height = "200px"
+    videoPregunta.height = "450px";
+    videoPregunta.width = "350px";
+    videoPregunta.src = document.getElementById("inputVideo").value;
+    videoPregunta.allowFullscreen = true;
+    videoPregunta.title = "Adobe Flash Player";
+    videoPregunta.wmode = "transparent";
+    videoPregunta.type = "application/x-shockwave-flash"
 
     btnBorrar.style.marginRight = "20px";
     btnRespuestas.style.marginRight = "20px";
